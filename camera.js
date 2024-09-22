db55ae3475db1d5c1dda6a5a0236f85c856a865c
cmderror <cmderror@icloud.com>
2024-09-22 12:09:44 +0000

    added main() for return pxls

--- camera.js
+++ camera.js
@@ -1,9 +1,9 @@
-
 class VideoConcerateAndFit{
-  videoMap = []
-  constructor(dim, resolution) {
-   	this.dim=dim;
-    this.res=resolution
+  
+  constructor(resolution=0.4, color_dim=1) {
+    this.videoMap = [];
+   	this.cdim=color_dim;
+    this.res=resolution;
   }
 	async start(){
     this.video = document.createElement("video");
@@ -25,50 +25,64 @@ class VideoConcerateAndFit{
     console.log(this.video.videoWidth, this.video.videoHeight)
     canvas.width = this.video.videoWidth;
     canvas.height = this.video.videoHeight;
-    
     ctx.drawImage(this.video, 0, 0);
-    const imgData = ctx.getImageData(0,0, canvas.width, canvas.height);
-    console.log(imgData.data)
-// 		videoMap.push(imgData.data);
+//     const imgData = ctx.getImageData(0,0, canvas.width, canvas.height);
+//     console.log(imgData.data)
+    this.videoMap.push(canvas.toDataURL('image/jpeg', this.res));
+  }
+
+}
+
+class Storage{
+  constructor(){
+    
+  }
+
+
+  button(){
+
   }
 }
 
 
-let btn = document.createElement("button")
-btn.innerText="Hold And Speek"
 
-let vid;
-btn.onmousedown = e => {
-	vid.capture()
-}
-btn.onmouseup = e => {
-	vid.stop()
-}
-
-let start = document.createElement("button")
-let btn_type=0
-let end=true
-start.innerText="To Camera =>";
-start.onclick = async () => {
-  if (!end){
-    return
+function main(){
+  let btn = document.createElement("button")
+  btn.innerText="Hold And Speek"
+  btn.onmousedown = e => {
+    vid.capture();
   }
-  end = false
-  btn_type = btn_type*(-1)+1
-
-  if (btn_type) {
-    vid = new VideoConcerateAndFit(0,0);
-    await vid.start();
-    start.innerText="<= exit camera";
-    document.body.appendChild(btn);
-    end=true
-  } else {
-    vid.stop();
-    start.innerText="To Camera =>";
-    document.body.removeChild(btn);
-    end=true
+  btn.onmouseup = e => {
+    vid.stop()
   }
-  start.innerText = btn_type ? "<= exit camera" : "To Camera =>";
+  let vid;
+  let start = document.createElement("button")
+  let btn_type=0
+  let end=true
+  start.innerText="To Camera =>";
+  start.onclick = async () => {
+    if (!end)
+      return
+    
+    end = false
+    btn_type = btn_type*(-1)+1
+
+    if (btn_type) {
+      vid = new VideoConcerateAndFit();
+      await vid.start();
+      start.innerText="<= exit camera";
+      document.body.appendChild(btn);
+      end=true
+    } else {
+      vid.stop();
+      start.innerText="To Camera =>";
+      document.body.removeChild(btn);
+      end=true
+      return vid.VideoMap
+    }
+    start.innerText = btn_type ? "<= exit camera" : "To Camera =>";
+  }
+
+  document.body.appendChild(start);
 }
 
-document.body.appendChild(start);
